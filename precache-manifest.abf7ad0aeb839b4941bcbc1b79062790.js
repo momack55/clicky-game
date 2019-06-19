1209a9cea7f971d8a9a96d7664f1cc58529d580e
momack55 <memccormack55@gmail.com>
2019-06-18 19:46:10 -0500

    Updates

---
+++ precache-manifest.abf7ad0aeb839b4941bcbc1b79062790.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ad1e27e227b05e09e245e5e123b0ac38",
+    "url": "/clicky-game/index.html"
+  },
+  {
+    "revision": "bfcbe3fe839dd1e9f83d",
+    "url": "/clicky-game/static/css/main.c9127f69.chunk.css"
+  },
+  {
+    "revision": "21475265196432d20b58",
+    "url": "/clicky-game/static/js/2.8cac1a3c.chunk.js"
+  },
+  {
+    "revision": "bfcbe3fe839dd1e9f83d",
+    "url": "/clicky-game/static/js/main.2b506d03.chunk.js"
+  },
+  {
+    "revision": "265538ab0e4606d350e6",
+    "url": "/clicky-game/static/js/runtime~main.27874bd3.js"
+  },
+  {
+    "revision": "9801a7459bdc56ea0352864962f306bf",
+    "url": "/clicky-game/static/media/headerImage.9801a745.jpeg"
+  }
+]);
